74ba991009fdca919d0944aad6deea43f14eab0a
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2024-04-06 19:39:34 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public int Id {  get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
