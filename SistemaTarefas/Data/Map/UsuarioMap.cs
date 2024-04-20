fb80313b7d1528b51f1974740cea21c59fed4cff
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2024-04-20 18:43:36 -0300

    Map: Tarefa e usuário

---
+++ SistemaTarefas/Data/Map/UsuarioMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaTarefas.Models;
+
+namespace SistemaTarefas.Data.Map
+{
+    public class UsuarioMap : IEntityTypeConfiguration<UsuarioModel>
+    {
+        void IEntityTypeConfiguration<UsuarioModel>.Configure(EntityTypeBuilder<UsuarioModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(150);
+        }
+    }
+
+}
--- SistemaTarefas/Models/TarefaModel.cs
+++ SistemaTarefas/Models/TarefaModel.cs
@@ -1,10 +1,12 @@
-﻿namespace SistemaTarefas.Models
+﻿using SistemaTarefas.Enums;
+
+namespace SistemaTarefas.Models
 {
     public class TarefaModel
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
-        public int Status { get; set; }
+        public StatusTarefa Status { get; set; }
     }
 }
